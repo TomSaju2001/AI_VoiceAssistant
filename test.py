861bfbb23e05106199726ef7ab2b388fefdc2346
Tom <tomsaju33@gmail.com>
2022-02-08 13:36:24 +0530

    Added Time Function

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import pyttsx3
-
+import datetime
 engine=pyttsx3.init()
 
 #Voice and Speech rate
@@ -9,9 +9,14 @@ engine.setProperty('voice', assistantVoice[0].id)
 newSpeechRate=180
 engine.setProperty('rate', newSpeechRate)
 
-#speak function
+#Speak function
 def speak(audio):
     engine.say(audio)
     engine.runAndWait()
 
-speak("Hello World! My name is Jarvis.")
+#Time function
+def time():
+    currentTime=datetime.datetime.now().strftime("%I:%M %p")
+    speak(currentTime)
+
+time()
