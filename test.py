492e841e4d1743763149a0bf56b6fd9bc8e76d75
Tom <tomsaju33@gmail.com>
2022-02-08 13:22:36 +0530

    Added Voice Changer and rate

--- test.py
+++ test.py
@@ -2,9 +2,16 @@ import pyttsx3
 
 engine=pyttsx3.init()
 
+#Voice and Speech rate
+assistantVoice=engine.getProperty('voices')
+engine.setProperty('voice', assistantVoice[0].id)
+
+newSpeechRate=180
+engine.setProperty('rate', newSpeechRate)
+
 #speak function
 def speak(audio):
     engine.say(audio)
     engine.runAndWait()
 
-speak("Tom Saju")
+speak("Hello World! My name is Jarvis.")
